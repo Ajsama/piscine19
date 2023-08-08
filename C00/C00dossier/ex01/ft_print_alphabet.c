06786d19fc53cf102b7c31fb993bf7f73ba1461c
Abiel-Jason Anson <aanson@fu-r1-p1.s19.be>
2023-08-08 20:13:59 +0200

    depuis 19

--- C00/C00dossier/ex01/ft_print_alphabet.c
+++ C00/C00dossier/ex01/ft_print_alphabet.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abiel-jason <abiel-jason@student.42.fr>    +#+  +:+       +#+        */
+/*   By: aanson <aanson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 17:09:05 by aanson            #+#    #+#             */
-/*   Updated: 2023/08/08 19:51:42 by abiel-jason      ###   ########.fr       */
+/*   Updated: 2023/08/08 20:13:05 by aanson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ void	ft_print_alphabet(void)
 {
 	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
-test from mac 
+test from school
+
 
